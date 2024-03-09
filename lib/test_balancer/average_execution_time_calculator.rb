3cfab07f2da8f2f35cd82759b801c8aadc5bc13d
Philipp Winkler <philrigid@gmail.com>
2024-03-09 09:36:55 +0100

    feat: add average execution time calculator for multiple test executions with different execution times

---
+++ lib/test_balancer/average_execution_time_calculator.rb
@@ -0,0 +1,108 @@
+class TestBalancer::AverageExecutionTimeCalculator
+  class AverageExecutionTimeCalculatorError < StandardError; end
+
+  private
+
+  attr_reader :test_cases, :test_cases_id
+
+  public
+
+  def initialize
+    @test_cases = {}
+    @test_cases_id = 0
+  end
+
+  # @param tests_with_execution_time [Array<Hash>]
+  # example: [{ test_path: 'test/first_test.rb', execution_time: 0.1 }, { test_path: 'test/second_test.rb', execution_time: 0.2 }]
+  # return void
+  def add_test_cases(tests_with_execution_time:)
+    @test_cases[test_cases_id] = tests_with_execution_time
+    @test_cases_id += 1
+  end
+
+  def reset
+    @test_cases = {}
+    @test_cases_id = 0
+  end
+
+  # return [Array<Hash>]
+  # example: [{ test_path: 'test/first_test.rb', execution_time: 0.1 }, { test_path: 'test/second_test.rb', execution_time: 0.2 }]
+  def medianize
+    raise AverageExecutionTimeCalculatorError, 'No test cases added' if test_cases.empty?
+
+    result = []
+
+    test_cases[0].each do |test_case|
+      test_path = test_case[:test_path]
+      result << { test_path:, execution_time: median_time_for_test_path(test_path:) }
+    end
+
+    result
+  end
+
+  # return [Array<Hash>]
+  # example: [{ test_path: 'test/first_test.rb', execution_time: 0.1 }, { test_path: 'test/second_test.rb', execution_time: 0.2 }]
+  def trimeanize
+    raise AverageExecutionTimeCalculatorError, 'No test cases added' if test_cases.empty?
+
+    result = []
+
+    test_cases[0].each do |test_case|
+      test_path = test_case[:test_path]
+      result << { test_path:, execution_time: trimean_time_for_test_path(test_path:) }
+    end
+
+    result
+  end
+
+  private
+
+  def median_time_for_test_path(test_path:)
+    test_case_execution_times = @test_cases.map do |_, test_cases|
+      test_cases.find { |test_case| test_case[:test_path] == test_path }[:execution_time]
+    end
+
+    median(numbers: test_case_execution_times)
+  end
+
+  def trimean_time_for_test_path(test_path:)
+    test_case_execution_times = @test_cases.map do |_, test_cases|
+      test_cases.find { |test_case| test_case[:test_path] == test_path }[:execution_time]
+    end
+
+    trimean(numbers: test_case_execution_times)
+  end
+
+  def median(numbers:)
+    return nil if numbers.empty?
+    sorted = numbers.sort
+    len = sorted.length
+    (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+  end
+
+  def trimean(numbers:)
+    numbers.sort!
+    n = numbers.length
+    q1 = quantile(numbers:, p: 0.25)
+    q3 = quantile(numbers:, p: 0.75)
+    m = median(numbers:)
+    lower_half = numbers.select { |x| x <= m }
+    upper_half = numbers.select { |x| x >= m }
+    t1 = median(numbers: lower_half)
+    t2 = median(numbers: upper_half)
+    (t1 + 2*m + t2) / 4.0
+  end
+
+  def quantile(numbers:, p:)
+    n = numbers.length
+    k = (n * p).floor
+    if k < 1
+      numbers.first
+    elsif k >= n
+      numbers.last
+    else
+      d = n * p - k
+      numbers[k-1] + d * (numbers[k] - numbers[k-1])
+    end
+  end
+end
---
+++ test/lib/test_balancer/average_execution_time_calculator_test.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module TestBalancer
+  class AverageExecutionTimeCalculatorTest < Minitest::Test
+    def test_medianize_for_single_execution_time
+      median_distributor = ::TestBalancer::AverageExecutionTimeCalculator.new
+      median_distributor.add_test_cases(
+        tests_with_execution_time: [
+          { test_path: "test/first_test.rb", execution_time: 0.1 },
+          { test_path: "test/second_test.rb", execution_time: 0.2 }
+        ]
+      )
+      result = median_distributor.medianize
+
+      assert_execution_times(result:, time1: 0.1, time2: 0.2)
+    end
+
+    def test_medianize_for_multiple_execution_times
+      median_distributor = create_multiple_execution_times
+      result = median_distributor.medianize
+
+      assert_execution_times(result:, time1: 11, time2: 12)
+    end
+
+    def test_trimean_time_for_multiple_execution_times
+      median_distributor = create_multiple_execution_times
+      result = median_distributor.trimeanize
+
+      assert_execution_times(result:, time1: 19.0, time2: 19.75)
+    end
+
+    def test_medianize_raises_error_when_no_test_cases_added
+      median_distributor = ::TestBalancer::AverageExecutionTimeCalculator.new
+      assert_raises ::TestBalancer::AverageExecutionTimeCalculator::AverageExecutionTimeCalculatorError do
+        median_distributor.medianize
+      end
+    end
+
+    def test_trimeanize_raises_error_when_no_test_cases_added
+      median_distributor = ::TestBalancer::AverageExecutionTimeCalculator.new
+      assert_raises ::TestBalancer::AverageExecutionTimeCalculator::AverageExecutionTimeCalculatorError do
+        median_distributor.trimeanize
+      end
+    end
+
+    private
+
+    def assert_execution_times(result:, time1:, time2:)
+      assert_equal time1, result[0][:execution_time]
+      assert_equal time2, result[1][:execution_time]
+    end
+
+    def create_multiple_execution_times
+      median_distributor = ::TestBalancer::AverageExecutionTimeCalculator.new
+      median_distributor.add_test_cases(
+        tests_with_execution_time: [
+          { test_path: "test/first_test.rb", execution_time: 5 },
+          { test_path: "test/second_test.rb", execution_time: 3 }
+        ]
+      )
+      test_paths = %w[test/first_test.rb test/second_test.rb]
+      execution_times = [
+        [1, 2],
+        [1, 2],
+        [4, 5],
+        [6, 7],
+        [8, 9],
+        [10, 11],
+        [12, 13],
+        [14, 15],
+        [16, 17],
+        [49, 50],
+        [51, 52],
+        [51, 52],
+        [51, 52]
+      ]
+      execution_times.length.times do |i|
+        median_distributor.add_test_cases(
+          tests_with_execution_time: [
+            { test_path: test_paths[0], execution_time: execution_times[i][0] },
+            { test_path: test_paths[1], execution_time: execution_times[i][1] }
+          ]
+        )
+      end
+      median_distributor
+    end
+  end
+end
